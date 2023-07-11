993a6ea0719c7d7bef8493ad9e39b68168bd1cc6
Lena Munad <munadlena@gmail.com>
2023-07-11 16:59:12 -0700

    Implemented to-do list APP front-end

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -1,3 +1,5 @@
+import Axios from "axios";
+
 import React, { Component } from "react";
 import { Button, TextField } from "@mui/material";
 import { DesktopDatePicker , LocalizationProvider} from '@mui/x-date-pickers';
@@ -39,6 +41,25 @@ class AddTodo extends Component {
   handleSubmit = (event) => {
     event.preventDefault();
     if (this.state.content.trim()) {
+
+      const jsonObject = {
+        id: this.state.id,
+        task: this.state.content,
+        currentDate: this.state.date,
+        dueDate: this.state.duedate
+     };
+
+      Axios({
+        method: "POST",
+        url: "http://localhost:8080/add/item",
+        data: {jsonObject},
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }).then(res => {
+        console.log(res.data.message);
+      });
+
       this.props.addTodo(this.state);
       this.setState({
         content: "",
@@ -47,6 +68,7 @@ class AddTodo extends Component {
       });
     }
   };
+
   render() {
     return (
       // 1. When rendering a component, you can render as many elements as you like as long as it is wrapped inside
