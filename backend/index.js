66e3c779be8cfea9d823c2a4f2b5c14c67118529
Lena Munad <munadlena@gmail.com>
2023-07-11 11:57:36 -0700

    Added the backend files

---
+++ backend/index.js
@@ -0,0 +1,43 @@
+
+const express = require("express"),
+       app = express(),
+       port = process.env.PORT || 8080,
+       cors = require("cors");
+const bodyParser = require('body-parser');
+const fs = require("fs").promises;
+
+app.use(cors());
+app.use(bodyParser.json({ extended: true }));
+app.listen(port, () => console.log("Backend server live on " + port));
+
+app.get("/", (req, res) => {
+    res.send({ message: "Connected to Backend server!" });
+});
+
+app.post("/add/item", addItem)
+
+async function addItem (request, response) {
+    try {
+        // Converting Javascript object (Task Item) to a JSON string
+        const id = request.body.jsonObject.id
+        const task = request.body.jsonObject.task
+        const curDate = request.body.jsonObject.currentDate
+        const dueDate = request.body.jsonObject.dueDate
+        const newTask = {
+          ID: id,
+          Task: task,
+          Current_date: curDate,
+          Due_date: dueDate
+        }
+
+        const data = await fs.readFile("database.json");
+        const json = JSON.parse(data);
+        json.push(newTask);
+        await fs.writeFile("database.json", JSON.stringify(json))
+        console.log('Successfully wrote to file') 
+        response.sendStatus(200)
+    } catch (err) {
+        console.log("error: ", err)
+        response.sendStatus(500)
+    }
+}
